6fce92e1f963523bd0e4a68853af48d05758054f
Mikey Hogarth <mikehogarth20@hotmail.com>
2011-12-19 21:57:18 +0000

    modularize

--- lib/tablecloth/ingredient.rb
+++ lib/tablecloth/ingredient.rb
@@ -1,44 +1,48 @@
-class Ingredient
-  attr_accessor :qty, :unit, :item
-
-  def initialize(params)
-    @qty = params[:qty]
-    @unit = params[:unit]
-    @item = params[:item]
-
-    @qty = 1 if @qty == :na && @unit != :na
-  end
-
-  #alias for qty
-  def quantity
-    @qty
-  end
-
-  #alias for unit
-  def units
-    @unit
-  end
-
-  #simple comparison operator
-  def ==(other_ingredient)
-      @qty == other_ingredient.qty &&
-      @unit == other_ingredient.unit &&
-      @item == other_ingredient.item
-  end
-
-  #returns true if this ingredient did not specify a qty or a unit
-  def is_free_text?
-    @qty == :na && @unit == :na
-  end
-
-  #This method formats an ingredient into a readable string
-  def to_s
-    #singular
-    return "#{@qty} #{@unit} of #{item}" if @qty == "1"
+module TableCloth
+  
+  class Ingredient
+    attr_accessor :qty, :unit, :item
     
-    #basic pluralization
-    "#{@qty} #{@unit}s of #{item}"
+    def initialize(params)
+      @qty = params[:qty]
+      @unit = params[:unit]
+      @item = params[:item]
+      
+      @qty = 1 if @qty == :na && @unit != :na
+    end
+    
+    #alias for qty
+    def quantity
+      @qty
+    end
+    
+    #alias for unit
+    def units
+      @unit
+    end
+    
+    #simple comparison operator
+    def ==(other_ingredient)
+      @qty == other_ingredient.qty &&
+        @unit == other_ingredient.unit &&
+        @item == other_ingredient.item
+    end
+    
+    #returns true if this ingredient did not specify a qty or a unit
+    def is_free_text?
+      @qty == :na && @unit == :na
+    end
+    
+    #This method formats an ingredient into a readable string
+    def to_s
+      #singular
+      return "#{@qty} #{@unit} of #{item}" if @qty == "1"
+      
+      #basic pluralization
+      "#{@qty} #{@unit}s of #{item}"
+    end
+  
+    
   end
   
-
 end
---
+++ lib/tablecloth/recipe.rb
@@ -0,0 +1,96 @@
+#Base class
+module TableCloth
+  class Recipe
+    
+    attr_reader :ingredients
+    
+    def initialize *args
+      
+      units_hash = YAML.load_file("lib/tablecloth/yaml/qty.yaml")
+      
+      @ingredients = []
+      
+      return if args.size == 0
+      
+      raise ArgumentError if args.size > 1
+      
+      args.first.each_line do |line|
+        @ingredients << parse_single(line, units_hash)
+      end    
+      
+    end
+    
+    #add ingredient straight into tablecloth instance
+    def << ingredient
+      
+      raise TypeError if (ingredient.class != Ingredient)
+      
+      @ingredients << ingredient
+    end
+    
+    #NOT YET IMPLEMENTED: will return entire ingredient list an html ul, can be overriden to include html attributes
+    def to_html
+      #TODO
+    raise NotImplementedError
+    end
+    
+    private
+    
+    #parse a single line from the ingredients list
+    def parse_single str, units_hash
+      
+      str = presub str
+      
+      qty = get_qty str
+      units = get_units str, units_hash
+      item = get_item str, units != :na
+      
+      Ingredient.new :qty => qty, :unit => units, :item => item   
+      
+  end
+    
+    #extrac the quantity from the string
+    def get_qty str
+      
+      #check for fractions first, then single units
+      qty = str.slice(/\d+\/\d+/) || str.slice(/\d+/)
+      
+      if qty == nil
+        :na
+      else
+        qty
+      end
+    end
+    
+    #extract the units from the string
+    def get_units str, units_hash
+      first_word = str.gsub(/\d+/, "").scan(/[a-zA-Z]+/).first
+      if units_hash.keys.include? first_word
+        units_hash[first_word]
+      else
+        :na
+      end
+    end
+    
+    #extract the item itself from the string
+    def get_item str,has_units
+      if has_units
+        #assume the first word is the unit
+        str.scan(/[a-zA-Z]+/).drop(1).join(" ")
+      else
+        #assume all non-numerical words are the item
+        str.gsub(/\d+/,"").scan(/\w+/).join(" ")
+      end
+    end
+    
+    #processes any pre-substitutions to the string 
+    def presub str
+      #TODO: there's gotta be a better way of doing things than this!
+      str.gsub!(/of/, "") #deal with "1 cup of sugar"
+      str.gsub!(/^(a )/, "1") #turn "a cup of sugar" into "1 cup of sugar"
+      str.gsub!(/ a /,"") #deal with "1/2 a cup of sugar"
+    str
+    end
+    
+  end
+end
